280f8ee4a18f201ebfc3dedf83749d5dd19694c7
Daisy <daisy.turcotte@live.ca>
2021-09-24 11:01:53 -0400

    Fix tests

--- backend/apps/offer/serializers.py
+++ backend/apps/offer/serializers.py
@@ -4,6 +4,13 @@ from django.contrib.auth.models import User
 
 
 class OfferSerializer(serializers.ModelSerializer):
+
+    def validate_creation(self, value):
+        ModelClass = self.Meta.model
+        if ModelClass.objects.filter(user=value).exists():
+            raise serializers.ValidationError('already exists')
+        return value
+
     class Meta:
         model = Offer
         fields = '__all__'
