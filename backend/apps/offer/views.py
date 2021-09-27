6ed40b80f07a81171ea532fe8e0f8d9e48733ebc
DTurc <49719906+DTurc@users.noreply.github.com>
2021-09-26 22:02:58 -0400

    Us4 ajout donnees bd (#6)
    * Création d'une base pour la page de l'employé
    
    * Création d'une base pour la page de l'employé
    
    * Refactorisation de la page employé et du navbar
    
    * Refactorisation de la page employé et du navbar
    
    * us2 - Ajout des fichiers de départ.
    
    * Définition du modèle Offer, de son serializer et de sa view.
    
    * Ajout des tests unitaires
    
    * Fix tests
    
    * Ajout du chargement dans la page login et ajustement du menu dans le navbar
    
    * Fix tests
    
    * Ajout des components ActiveOffer et DetailedOffer
    
    * Ajout de fixtures
    
    * Modification des fixtures
    
    * Utilisateurs et offres valides
    
    Co-authored-by: Andrew <andrewduquette@gmail.com>
    Co-authored-by: Andrew <40619777+VP-0@users.noreply.github.com>
    Co-authored-by: Danic Fortier <51426753+DanicFortier@users.noreply.github.com>

---
+++ backend/apps/offer/views.py
@@ -0,0 +1,72 @@
+from django.db.models import Q
+from django.http import Http404
+
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.decorators import api_view  # Pour utilser annotations
+from django.contrib.auth.models import User
+
+from rest_framework import status
+from .models import Offer
+from .serializers import OfferSerializer, UserSerializer
+
+from rest_framework.decorators import api_view, schema
+from rest_framework.schemas import AutoSchema
+
+
+class Offers(APIView):
+
+    """
+    Récupèrer toutes les offres.
+    """
+
+    def get(self, request, format=None):
+        offers = Offer.objects.all()
+        serializer = OfferSerializer(offers, many=True)
+        return Response(serializer.data)
+
+    """
+    Création d'une offre.
+    """
+
+    def post(self, request, format=None):
+        serializer = OfferSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class OfferDetail(APIView):
+
+    """
+    Récupérer une offre selon l'id de l'offre.
+    """
+
+    def get_object(self, no_offer):
+        try:
+            return Offer.objects.get(id=no_offer)
+        except Offer.DoesNotExist:
+            raise Http404
+
+    def get(self, request, no_offer, format=None):
+        offer = self.get_object(no_offer)
+        serializer = OfferSerializer(offer)
+        return Response(serializer.data)
+
+
+class UserOffers(APIView):
+    """
+    Récupérer toutes les offres d'un employé.
+    """
+
+    def get_object(self, no_user):
+        try:
+            return Offer.objects.filter(id_user=no_user)
+        except User.DoesNotExist:
+            raise Http404
+
+    def get(self, request, no_user, format=None):
+        offer = self.get_object(no_user)
+        serializer = OfferSerializer(offer, many=True)
+        return Response(serializer.data)
--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,155 +1,230 @@
 <template>
   <div id="wrapper">
-    <nav class="navbar is-dark">
+    <nav class="navbar">
       <div class="navbar-brand">
-        <router-link to="/" class="navbar-item">
-          <strong>Communoservice</strong>
-        </router-link>
-
-        <a
-          href="#"
-          class="navbar-burger"
-          aria-label="menu"
-          aria-expanded="false"
-          data-target="navbar-menu"
-          @click="afficherMenuMobile = !afficherMenuMobile"
-        >
-          <span aria-label="true"></span>
-          <span aria-label="true"></span>
-          <span aria-label="true"></span>
+        <a class="navbar-item" href="/">
+          <h4 class="title is-4 has-text-weight-bold has-text-white">Communoservice</h4>
         </a>
+        <div class="navbar-burger" @click="showNav = !showNav" :class="{ 'is-active': showNav }">
+          <span></span>
+          <span></span>
+          <span></span>
+        </div>
       </div>
-
-      <div
-        class="navbar-menu"
-        id="navbar-menu"
-        v-bind:class="{ 'is-active': afficherMenuMobile }"
-      >
+      <div class="navbar-menu" :class="{ 'is-active': showNav }">
         <div class="navbar-start">
-          <div class="navbar-item">
-            <form action="/search" method="get">
-              <div class="field has-addons">
-                <div class="control">
-                  <input
-                    type="text"
-                    name="query"
-                    class="input"
-                    placeholder="Que recherchez-vous ?"
-                  />
-                </div>
-
-                <div class="control">
-                  <button class="button is-success">
-                    <span class="icon"><i class="fas fa-search"></i></span>
+          <a class="navbar-item" href="/" @click="showNav = !showNav">
+            Accueil
+          </a>
+          <a class="navbar-item" href="https://github.com/VP-0/communoservice" @click="showNav = !showNav">
+            <span class="mr-2">GitHub</span>
+            <span class="icon is-small">
+              <i class="fab fa-github"></i>
+            </span>
+          </a>
+        </div>
+        <div class="navbar-end">
+          <template v-if="$store.state.isAuthenticated">
+            <router-link to="/mon-compte" class="navbar-item" @click="showNav = !showNav">
+              <span class="mr-3">Mon compte</span>
+              <span class="icon is-small mr-3">
+                <i class="fas fa-user-circle"></i>
+              </span>
+            </router-link>
+            <div class="navbar-item">
+              <div class="dropdown is-hoverable" :class="dropdownRight ? 'is-right' : 'is-left'">
+                <div class="dropdown-trigger">
+                  <button class="button is-rounded has-text-centered" aria-haspopup="true"
+                    aria-controls="dropdown-menu3">
+                    <span class="icon is-small">
+                      <i class="fas fa-angle-down" aria-hidden="true"></i>
+                    </span>
                   </button>
                 </div>
+                <div class="dropdown-menu pu-2" id="dropdown-menu3" role="menu">
+                  <div class="dropdown-content has-text-left">
+                    <a href="#" class="dropdown-item" v-on:click="modalSettingsisActive = !modalSettingsisActive">
+                      <span class="icon is-small mr-3">
+                        <i class="fas fa-cog"></i>
+                      </span>
+                      <span> Paramêtres </span>
+                    </a>
+                    <hr class="dropdown-divider" />
+                    <a href="#" @click="logout()" class="dropdown-item has-text-danger">
+                      <span class="icon is-small mr-3">
+                        <i class="fas fa-sign-out-alt"></i>
+                      </span>
+                      <span> Déconnexion </span>
+                    </a>
+                  </div>
+                </div>
               </div>
-            </form>
-          </div>
-        </div>
-
-        <div class="navbar-end">
-          <div class="navbar-item">
-            <div class="buttons">
-              <template v-if="$store.state.isAuthenticated">
-                <router-link to="/mon-compte" class="button is-light">
-                  Mon compte
-                </router-link>
-              </template>
-
-              <template v-else>
-                <router-link to="/connexion" class="button is-light">
-                  Connexion
-                </router-link>
-              </template>
             </div>
-          </div>
+          </template>
+
+          <template v-else>
+            <router-link to="/connexion" class="navbar-item">
+              <span class="mr-3">Connexion</span>
+              <span class="icon is-small mr-3">
+                <i class="fas fa-sign-in-alt"></i>
+              </span>
+            </router-link>
+          </template>
         </div>
       </div>
     </nav>
-
-    <div
-      class="is-loading-bar has-text-centered"
-      v-bind:class="{ 'is-loading': $store.state.isLoading }"
-    >
-      <div class="lds-dual-ring"></div>
-    </div>
-
     <section class="section">
       <router-view />
     </section>
 
+    <div class="modal" v-bind:class="{ 'is-active': modalSettingsisActive}">
+      <div class="modal-background"></div>
+      <div class="modal-card">
+        <header class="modal-card-head">
+          <p class="modal-card-title">Paramêtres</p>
+          <button class="delete" v-on:click="modalSettingsisActive = !modalSettingsisActive"
+            aria-label="close"></button>
+        </header>
+        <section class="modal-card-body">
+          Configuration des paramêtres ici
+        </section>
+        <footer class="modal-card-foot">
+          <button class="button is-success w-100">Sauvegarder</button>
+          <button class="button is-danger w-100"
+            v-on:click="modalSettingsisActive = !modalSettingsisActive">Canceler</button>
+        </footer>
+      </div>
+    </div>
+
     <footer class="footer">
-      <p class="has-text-centered">
-        Copyright 
-        <i class="fas fa-copyright" aria-hidden="true"></i>
-        Communoservice 2021
-      </p>
+      <div class="content has-text-centered">
+        <p>
+          Copyright
+          <i class="fas fa-copyright" aria-hidden="true"></i>
+          Communoservice 2021
+        </p>
+      </div>
     </footer>
   </div>
 </template>
 
 <script>
-import axios from "axios";
+  import axios from "axios";
+  import {
+    toast
+  } from "bulma-toast";
+  export default {
+    data() {
+      return {
+        showNav: false,
+        window: {
+          width: 0,
+          height: 0
+        },
+        dropdownRight: true,
+        modalSettingsisActive: false,
+      };
+    },
+    beforeCreate() {
+      this.$store.commit("initializeStore");
 
-export default {
-  data() {
-    return {
-      afficherMenuMobile: false
-    };
-  },
-  beforeCreate() {
-    this.$store.commit("initializeStore");
+      const token = this.$store.state.token;
 
-    const token = this.$store.state.token;
-
-    if (token) {
-      axios.defaults.headers.common["Authorization"] = "Token " + token;
-    } else {
-      axios.defaults.headers.common["Authorization"] = "";
-    }
-  },
-  mounted() {
-  },
-  computed: {
-
-  }
-};
+      if (token) {
+        axios.defaults.headers.common["Authorization"] = "Token " + token;
+      } else {
+        axios.defaults.headers.common["Authorization"] = "";
+      }
+    },
+    mounted() {
+      window.addEventListener('resize', this.handleResize);
+      this.handleResize();
+    },
+    computed: {},
+    methods: {
+      logout() {
+        axios.defaults.headers.common["Authorization"] = "";
+        localStorage.removeItem("token");
+        localStorage.removeItem("username");
+        localStorage.removeItem("userid");
+        this.$store.commit("removeToken");
+        this.$router.push("/");
+        toast({
+          message: "Déconnecté avec succès!",
+          type: "is-success",
+          dismissible: false,
+          pauseOnHover: false,
+          duration: 3000,
+          position: "center",
+          animate: {
+            in: "fadeInRightBig",
+            out: "fadeOutLeftBig",
+          },
+        });
+      },
+      handleResize() {
+        this.window.width = window.innerWidth;
+        this.window.height = window.innerHeight;
+        if (window.innerWidth > 1006) {
+          this.showNav = false;
+          this.dropdownRight = true;
+        } else {
+          this.dropdownRight = false;
+        }
+      },
+    },
+  };
 </script>
 
 <style lang="scss">
-@import "../node_modules/bulma";
-.lds-dual-ring {
-  display: inline-block;
-  width: 80px;
-  height: 80px;
-}
-.lds-dual-ring:after {
-  content: " ";
-  display: block;
-  width: 64px;
-  height: 64px;
-  margin: 8px;
-  border-radius: 50%;
-  border: 6px solid #ccc;
-  border-color: #ccc transparent #ccc transparent;
-  animation: lds-dual-ring 1.2s linear infinite;
-}
-@keyframes lds-dual-ring {
-  0% {
-    transform: rotate(0deg);
+  @import "../node_modules/bulmaswatch/flatly/bulmaswatch.scss";
+
+  .test-classe {
+    @extend .title;
+    @extend .is-4;
+    @extend .has-text-centered;
   }
-  100% {
-    transform: rotate(360deg);
+
+  .navbar {
+    border-radius: 0 !important;
   }
-}
-.is-loading-bar {
-  height: 0;
-  overflow: hidden;
-  -webkit-transition: all 0.3s;
-  transition: all 0.3s;
-  &.is-loading {
+
+  .lds-dual-ring {
+    display: inline-block;
+    width: 80px;
     height: 80px;
   }
-}
+
+  .lds-dual-ring:after {
+    content: " ";
+    display: block;
+    width: 64px;
+    height: 64px;
+    margin: 8px;
+    border-radius: 50%;
+    border: 6px solid #ccc;
+    border-color: #ccc transparent #ccc transparent;
+    animation: lds-dual-ring 1.2s linear infinite;
+  }
+
+  @keyframes lds-dual-ring {
+    0% {
+      transform: rotate(0deg);
+    }
+
+    100% {
+      transform: rotate(360deg);
+    }
+  }
+
+  .is-loading-bar {
+    height: 0;
+    overflow: hidden;
+    -webkit-transition: all 0.3s;
+    transition: all 0.3s;
+
+    &.is-loading {
+      height: 80px;
+    }
+  }
 </style>
