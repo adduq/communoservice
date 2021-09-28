ba384844bd264f80d3488bb0419b0e740f7e1752
Daisy <daisy.turcotte@live.ca>
2021-09-28 16:32:42 -0400

    Endpoint recherche d'offres

--- backend/apps/offer/urls.py
+++ backend/apps/offer/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('offers/', views.Offers.as_view()),
     path('offers/<int:no_offer>/', views.OfferDetail.as_view()),
     path('<int:no_user>/', views.UserOffers.as_view()),
+    path('offers/search', views.search)
 ]
