e5d8376117683a9d1fdfd3839b97af830993ce1c
Andrew <andrewduquette@gmail.com>
2021-10-05 20:36:17 -0400

    Ajout du endpoint userinfo/me/

--- backend/apps/userinfo/urls.py
+++ backend/apps/userinfo/urls.py
@@ -4,4 +4,5 @@ from apps.userinfo import views
 
 urlpatterns = [
     path('userinfo/<int:user_id>/', views.UserInfoDetail.as_view()),
+    path('userinfo/me/', views.MyUserInfo.as_view()),
 ]
--- backend/apps/userinfo/views.py
+++ backend/apps/userinfo/views.py
@@ -1,5 +1,5 @@
 from django.db.models import Q
-from django.http import Http404, JsonResponse
+from django.http import Http404, HttpResponse, JsonResponse
 from django.forms.models import model_to_dict
 
 from rest_framework.views import APIView
@@ -34,10 +34,45 @@ class UserInfoDetail(APIView):
             out_dict = {'is_active':user_is_active}
             out_dict.update(serializer.data)
 
+            del out_dict['id']
+            del out_dict['email']
+            del out_dict['profile_is_completed']
+
             return out_dict
         except UserInfo.DoesNotExist:
             raise Http404
 
     def get(self, request, user_id, format=None):
         userinfo = self.get_object(user_id)
+        return Response(userinfo)
+
+class MyUserInfo(APIView):
+
+    """
+    Récupérer l'info de l'utilisateur à l'aide de son token.
+    """
+
+    def get_object(self, user_id):
+        try:
+
+            user = UserInfo.objects.get(user_id=user_id)
+            serializer = UserInfoSerializer(user)
+
+            out_dict = {}
+            out_dict.update(serializer.data)
+            
+            del out_dict['id']
+            del out_dict['email']
+            del out_dict['profile_is_completed'] 
+
+            return out_dict
+        except UserInfo.DoesNotExist:
+            raise Http404
+
+    def get(self, request, format=None):
+
+        if request.user.is_anonymous:
+            return HttpResponse('Unauthorized', status=401)
+
+        userinfo = self.get_object(request.user.id)
         return Response(userinfo)
