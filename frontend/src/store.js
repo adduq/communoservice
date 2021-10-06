df8be8a93e8107449df395f4da83d085e0948072
Andrew <andrewduquette@gmail.com>
2021-10-05 21:31:00 -0400

    Ajout du persistedstate pour garder l'état du store entre les vues

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,4 +1,5 @@
 import { createStore } from "vuex";
+import createPersistedState from "vuex-persistedstate";
 
 export default createStore({
     state: {
@@ -27,4 +28,5 @@ export default createStore({
     },
     actions: {},
     modules: {},
+    plugins: [createPersistedState()]
 });
