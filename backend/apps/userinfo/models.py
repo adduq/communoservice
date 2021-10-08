2ed98cdaebe96874b534814b87d61b436eaea3f2
Andrew <andrewduquette@gmail.com>
2021-10-08 13:40:41 -0400

    Ajout de la fonctionalité is_online et ajustement de la verbosité en dev

--- backend/apps/userinfo/models.py
+++ backend/apps/userinfo/models.py
@@ -3,28 +3,22 @@ from django.contrib.auth.models import User
 from django.contrib.auth.signals import user_logged_in, user_logged_out
 from django.dispatch import receiver
 import logging
-import pprint
+from pprint import pprint
 
 logger = logging.getLogger(__name__)
 
 
 @receiver(user_logged_in)
-def login_logger(request, user, **kwargs):
-    print('Logged in')
-    pprint(vars(user))
-    userinfo = UserInfo.objects.get(user_id=request.user.user_id)
-    pprint(vars(userinfo))
-    # userinfo.is_online = True
-    # userinfo.save()
+def login_logger(sender, request, user, **kwargs):
+    userinfo = UserInfo.objects.get(user_id=user.id)
+    userinfo.is_online = True
+    userinfo.save()
 
 @receiver(user_logged_out)
-def got_offline(request, user, **kwargs):   
-    print('Logged off')
-    pprint(vars(user))
-    userinfo = UserInfo.objects.get(user_id=request.user.user_id)
-    pprint(vars(userinfo))
-    # userinfo.is_online = False
-    # userinfo.save()
+def got_offline(sender, request, user, **kwargs):
+    userinfo = UserInfo.objects.get(user_id=user.id)
+    userinfo.is_online = False
+    userinfo.save()
 
 class UserInfo(models.Model):
     user_id = models.OneToOneField(User, on_delete=models.CASCADE, to_field="id")
@@ -57,10 +51,10 @@ class UserInfo(models.Model):
 
     nb_rating_as_employer = models.PositiveIntegerField(null=False, default=0)
 
-    user_bio = models.CharField(max_length=150)
+    user_bio = models.CharField(max_length=150, null=True)
 
-    location_lat = models.CharField(max_length=15)
+    location_lat = models.CharField(max_length=15, null=True)
 
-    location_lon = models.CharField(max_length=15)
+    location_lon = models.CharField(max_length=15, null=True)
 
-    address = models.CharField(max_length=100)
+    address = models.CharField(max_length=100, null=True)
