8d0c0f18a4023f355b7ddcd866d0979b5bb6c7b1
Pierrick Vittet <pierrick.vittet.13@gmail.com>
2021-11-05 15:03:08 -0400

    us3 - ajustement page admin v.2.4

--- backend/scripts/entrypoint.sh
+++ backend/scripts/entrypoint.sh
@@ -6,4 +6,4 @@ python3 manage.py collectstatic --no-input
 python3 manage.py loaddata apps/fixtures.json
 
 # uwsgi --http "0.0.0.0:${PORT}" --module config.wsgi:application --master --processes 4 --threads 2
-uwsgi --ini uwsgi.ini
+uwsgi --ini uwsgi.ini --http-socket "0.0.0.0:${PORT}"
