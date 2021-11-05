26436e4d48b03164bf81b75cfeb0d8ad9cbcb92a
Pierrick Vittet <pierrick.vittet.13@gmail.com>
2021-11-05 15:32:27 -0400

    us3 - ajustement page admin v.2.6

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -9,6 +9,14 @@ urlpatterns = [
     path('api/v1/', include('djoser.urls.authtoken')),
     path('api/v1/', include('apps.offer.urls')),
     path('api/v1/', include('apps.userinfo.urls'))
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+]
 
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+if settings.DEBUG:
+    urlpatterns = urlpatterns + \
+        static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns = urlpatterns + \
+        static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+# ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+# urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
