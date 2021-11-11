a9a5e26c84da1f2b4da11aa366b4aefafc4496e9
Pierrick Vittet <pierrick.vittet.13@gmail.com>
2021-11-11 01:27:27 -0500

    us3 - test pour la sécurisation du transfert de fichier static - v2.1

--- backend/scripts/entrypoint.sh
+++ backend/scripts/entrypoint.sh
@@ -5,4 +5,5 @@ python3 manage.py migrate --no-input
 python3 manage.py collectstatic --no-input
 python3 manage.py loaddata apps/fixtures.json
 
-uwsgi --ini uwsgi.ini --http-socket "0.0.0.0:${PORT}" --mime-file /usr/lib/python3.9/mimetypes.py
+uwsgi --ini uwsgi.ini --http-socket "0.0.0.0:${PORT}" --mime-file /etc/mime.types
+# uwsgi --ini uwsgi.ini --http-socket "0.0.0.0:${PORT}" --mime-file /usr/lib/python3.9/mimetypes.py
