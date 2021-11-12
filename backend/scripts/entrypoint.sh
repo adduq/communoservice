e1b0be54b5e9d10f517c3e75df9f45f3199382c4
Pierrick Vittet <pierrick.vittet.13@gmail.com>
2021-11-11 20:49:44 -0500

    us3 - Sécurisation du backend admin + merge de la dev en cours

--- backend/scripts/entrypoint.sh
+++ backend/scripts/entrypoint.sh
@@ -6,4 +6,3 @@ python3 manage.py collectstatic --no-input
 python3 manage.py loaddata apps/fixtures.json
 
 uwsgi --ini uwsgi.ini --http-socket "0.0.0.0:${PORT}" --mime-file /etc/mime.types
-# uwsgi --ini uwsgi.ini --http-socket "0.0.0.0:${PORT}" --mime-file /usr/lib/python3.9/mimetypes.py
