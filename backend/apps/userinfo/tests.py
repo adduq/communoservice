f0d02850ff96ebd2ae75c90d89e2ceddb0f6bfaf
Andrew <andrewduquette@gmail.com>
2021-11-24 23:35:21 -0500

    Début des tests pour le modèle userinfo

--- backend/apps/userinfo/tests.py
+++ backend/apps/userinfo/tests.py
@@ -7,7 +7,7 @@ from django.db import DataError
 from django.contrib.auth.models import User
 from .models import UserInfo
 #from .fixtures import OfferFixtures
-from .views import UserInfoDetail
+from django.urls import reverse 
 from pprint import pprint
 
 
@@ -17,5 +17,47 @@ from pprint import pprint
 
 # Tester le trigger dans la BD
 
-class TestOffer(unittest.TestCase):
-    pass
+class TestUserInfo(TestCase):
+    def setUp(self):
+         # RECRUITER DATASET
+        self.aragorn = User.objects.create(
+            username='aragorn',
+            email='aragorn@globetrotter.net'
+        )
+        self.aragorn.set_password('qwerty1234')
+        self.aragorn.save()
+        self.aragorn_userinfo = UserInfo.objects.create(
+            user_id=self.aragorn,
+            first_name='Aragorn',
+            last_name='LeBlanc',
+            profile_is_completed= True,
+            email='aragorn@globetrotter.net',
+            location_lat='46.848193',
+            location_lon='-71.138846',
+            address='837 Rte Prévost, Saint-Pierre, QC',
+        )
+    
+    def tearDown(self):
+        self.aragorn.delete()
+    
+    def test_UserInfoDetail_whenGetUserInfoAsOtherUser_thenCorrectInfoReturned(self):
+        login_data = {
+            'username': self.aragorn.username,
+            'password': 'qwerty1234',
+        }
+
+        response = self.client.post(reverse('login'), login_data, format='json')
+        token = response.data['auth_token']
+
+        headers = {
+           'HTTP_AUTHORIZATION': 'Token  ' + token
+        }
+        response = self.client.get(reverse('userinfo', kwargs={'user_id': self.aragorn.id}), **headers)
+        
+        self.assertEqual(response.status_code, 200)
+        self.assertTrue('first_name' in response.data and response.data['first_name'] == self.aragorn_userinfo.first_name)
+        self.assertTrue('last_name' in response.data and response.data['last_name'] == self.aragorn_userinfo.last_name)
+        self.assertTrue('user_id' in response.data and response.data['user_id'] == self.aragorn_userinfo.user_id.id)
+        self.assertFalse('id' in response.data)
+        self.assertFalse('profile_is_completed' in response.data)
+        self.assertFalse('email' in response.data)
--- backend/apps/userinfo/urls.py
+++ backend/apps/userinfo/urls.py
@@ -3,7 +3,7 @@ from django.urls import path, include
 from apps.userinfo import views
 
 urlpatterns = [
-    path('userinfo/<int:user_id>/', views.UserInfoDetail.as_view()),
+    path('userinfo/<int:user_id>/', views.UserInfoDetail.as_view(), name="userinfo"),
     path('userinfo/me/', views.MyUserInfo.as_view()),
     path('userinfo/me/update/', views.UpdateUserInfo.as_view()),
     path('userinfo/me/profile-image/', views.UpdateUserProfileImage.as_view()),
